808413180ab56919346e76f3005a8cb064476628
General-Monish <monissam28@gmail.com>
2026-01-22 05:19:08 +0530

    done with basic working like adding the todo

--- TO_DO_APP/todo.js
+++ TO_DO_APP/todo.js
@@ -1,6 +1,7 @@
 // TO_DO_APP/todo.js
 // This file contains the logic for a simple to-do application
 let toDoList = [];
+displayToDo();
 
 // Function to add a new to-do item
 function addTodo() {
@@ -10,4 +11,14 @@ function addTodo() {
     let inputValueElemet = inputButtonElement.value;
     toDoList.push(inputValueElemet);
     inputButtonElement.value = '';
+    displayToDo();
+}
+
+// Function to display the to-do list
+function displayToDo(){
+    let todoListItem = document.querySelector('#todo_list');
+    todoListItem.innerHTML = '';
+    for ( let i = 0; i < toDoList.length; i++){
+        todoListItem.innerHTML += `${toDoList[i]} <br/>`;
+    }
 }
