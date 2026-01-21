468fa879e4703c05149596adf404946303813118
General-Monish <monissam28@gmail.com>
2026-01-21 17:44:54 +0530

    added some comments

--- TO_DO_APP/todo.js
+++ TO_DO_APP/todo.js
@@ -1,7 +1,12 @@
+// TO_DO_APP/todo.js
+// This file contains the logic for a simple to-do application
 let toDoList = [];
 
+// Function to add a new to-do item
 function addTodo() {
+    // Get the input element and its value
     let inputButtonElement = document.querySelector('#todo_input');
+    // Get the value from the input element
     let inputValueElemet = inputButtonElement.value;
     toDoList.push(inputValueElemet);
     inputButtonElement.value = '';
