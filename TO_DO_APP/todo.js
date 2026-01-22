959f94d365d4851a2fe553b24de5ef5bcb2dc4eb
General-Monish <monissam28@gmail.com>
2026-01-22 05:42:02 +0530

    added delete button

--- TO_DO_APP/todo.js
+++ TO_DO_APP/todo.js
@@ -1,6 +1,6 @@
 // TO_DO_APP/todo.js
 // This file contains the logic for a simple to-do application
-let toDoList = [];
+let toDoList = ['Learn JavaScript', 'Build a to-do app', 'Profit!'];
 displayToDo();
 
 // Function to add a new to-do item
@@ -16,9 +16,18 @@ function addTodo() {
 
 // Function to display the to-do list
 function displayToDo(){
-    let todoListItem = document.querySelector('#todo_list');
-    todoListItem.innerHTML = '';
+    let todoListItem = document.querySelector('#todo_container');
+    let newHTML = '';
+
     for ( let i = 0; i < toDoList.length; i++){
-        todoListItem.innerHTML += `${toDoList[i]} <br/>`;
+        newHTML += `
+        <div>
+        <span>${toDoList[i]} </span>
+        <button onclick="toDoList.splice(${i},1);
+        displayToDo();
+        ">Delete</button>
+        </div>
+        `;
     }
+    todoListItem.innerHTML = newHTML;
 }
