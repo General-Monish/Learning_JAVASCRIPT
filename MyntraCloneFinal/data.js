520c015a8c7e320981e64bbce200e60ae2738076
General-Monish <monissam28@gmail.com>
2026-03-04 14:43:37 +0530

    adding to bag function and local storage implementation finished will work on bag page next

--- MyntraCloneFinal/data.js
+++ MyntraCloneFinal/data.js
@@ -1,5 +1,6 @@
 const items = [
   {
+    id : '001',
     item_Image: "/MyntraCloneFinal/Assets/Images/mine/1.jpg",
     rating: { stars: 4.5, reviews: "1400 Reviews" },
     company_Name: "GTA Designs",
@@ -7,52 +8,59 @@ const items = [
     pricing: { discounted_Price: 499, original_Price: 999, discount_Percentage: 50 }
   },
   {
+      id : '002',
     item_Image: "/MyntraCloneFinal/Assets/Images/mine/2.jpg",
     rating: { stars: 4.5, reviews: "1400 Reviews" },
-    company_Name: "GTA Designs",
-    item_Name: "GTA Wallpaper 4k",
-    pricing: { discounted_Price: 499, original_Price: 999, discount_Percentage: 50 }
+    company_Name: "Spider Man Designs",
+    item_Name: "Spider Man Wallpaper 4k",
+    pricing: { discounted_Price: 459, original_Price: 999, discount_Percentage: 50 }
   },
   {
+    id : '003',
     item_Image: "/MyntraCloneFinal/Assets/Images/mine/3.jpg",
     rating: { stars: 4.5, reviews: "1400 Reviews" },
-    company_Name: "GTA Designs",
-    item_Name: "GTA Wallpaper 4k",
-    pricing: { discounted_Price: 499, original_Price: 999, discount_Percentage: 50 }
+    company_Name: "GTA Car Designs",
+    item_Name: "GTA Car Wallpaper 4k",
+    pricing: { discounted_Price: 799, original_Price: 999, discount_Percentage: 50 }
   },
   {
+    id : '004',
     item_Image: "/MyntraCloneFinal/Assets/Images/mine/4.jpg",
     rating: { stars: 4.5, reviews: "1400 Reviews" },
-    company_Name: "GTA Designs",
-    item_Name: "GTA Wallpaper 4k",
-    pricing: { discounted_Price: 499, original_Price: 999, discount_Percentage: 50 }
+    company_Name: "Call Of Duty Designs",
+    item_Name: "Call Of Duty Wallpaper 4k",
+    pricing: { discounted_Price: 999, original_Price: 999, discount_Percentage: 50 }
   },
   {
+    id : '005',
     item_Image: "/MyntraCloneFinal/Assets/Images/mine/9.jpg",
     rating: { stars: 4.5, reviews: "1400 Reviews" },
-    company_Name: "GTA Designs",
-    item_Name: "GTA Wallpaper 4k",
-    pricing: { discounted_Price: 499, original_Price: 999, discount_Percentage: 50 }
+    company_Name: "God Of War Designs",
+    item_Name: "God Of War Wallpaper 4k",
+    pricing: { discounted_Price: 299, original_Price: 999, discount_Percentage: 50 }
   },
   {
+    id : '006',
     item_Image: "/MyntraCloneFinal/Assets/Images/mine/6.jpg",
     rating: { stars: 4.5, reviews: "1400 Reviews" },
-    company_Name: "GTA Designs",
-    item_Name: "GTA Wallpaper 4k",
-    pricing: { discounted_Price: 499, original_Price: 999, discount_Percentage: 50 }
+    company_Name: "GTA Car2 Designs",
+    item_Name: "GTA Car2 Wallpaper 4k",
+    pricing: { discounted_Price: 199, original_Price: 999, discount_Percentage: 50 }
   },
   {
+    id : '007',
     item_Image: "/MyntraCloneFinal/Assets/Images/mine/7.jpg",
     rating: { stars: 4.5, reviews: "1400 Reviews" },
-    company_Name: "GTA Designs",
-    item_Name: "GTA Wallpaper 4k",
-    pricing: { discounted_Price: 499, original_Price: 999, discount_Percentage: 0 }
+    company_Name: "God Of War Ragnarok Designs",
+    item_Name: "God Of War Ragnarok Wallpaper 4k",
+    pricing: { discounted_Price: 699, original_Price: 999, discount_Percentage: 0 }
   },
   {
+    id : '008',
     item_Image: "/MyntraCloneFinal/Assets/Images/mine/8.jpg",
     rating: { stars: 0, reviews: "0 Reviews" },
-    company_Name: "GTA Designs",
-    item_Name: "GTA Wallpaper 4k",
-    pricing: { discounted_Price: 499, original_Price: 999, discount_Percentage: 50 }
+    company_Name: "Naruto Designs",
+    item_Name: "Naruto Zabusa Wallpaper 4k",
+    pricing: { discounted_Price: 399, original_Price: 999, discount_Percentage: 50 }
   }
 ];
--- MyntraCloneFinal/script.js
+++ MyntraCloneFinal/script.js
@@ -1,9 +1,35 @@
-let itemsContainerElement = document.querySelector('.items-Container');
-let innerHTMLString = '';
-console.log(items);
-console.log(typeof items);
-items.forEach(item => {
-    innerHTMLString += `<div class="item-Container">
+let BagItems;
+onLoadMethod();
+
+function onLoadMethod() {
+    let bagItemsFromLocalStorageStr = localStorage.getItem('BagItems');
+    BagItems = bagItemsFromLocalStorageStr ? JSON.parse(bagItemsFromLocalStorageStr) : [];
+    DisplayItemsOnHomePage();
+    DisplayCartItems();
+}
+function AddToCartMethod(itemID) {
+    BagItems.push(itemID);
+    localStorage.setItem('BagItems', JSON.stringify(BagItems));
+    DisplayCartItems();
+}
+
+function DisplayCartItems() {
+    let bagItemCountElement = document.querySelector('.bag-item-count');
+    if (BagItems.length > 0) {
+        bagItemCountElement.style.visibility = 'visible';
+        bagItemCountElement.innerText = BagItems.length;
+    } else {
+        bagItemCountElement.style.visibility = 'hidden';
+    }
+}
+
+function DisplayItemsOnHomePage() {
+    let itemsContainerElement = document.querySelector('.items-Container');
+    let innerHTMLString = '';
+    console.log(items);
+    console.log(typeof items);
+    items.forEach(item => {
+        innerHTMLString += `<div class="item-Container">
                 <img class="item-image-1" src="${item.item_Image}" alt="mine-image-1">
                 <div class="rating">
                     ${item.rating.stars} ⭐ | ${item.rating.reviews}
@@ -19,7 +45,8 @@ items.forEach(item => {
                     <span class="original-price">₹ ${item.pricing.original_Price}</span>
                     <span class="discount-percentage">(${item.pricing.discount_Percentage} % OFF)</span>
                 </div>
-                <button class="btn-Add-To-Cart">Add to Cart</button>
+                <button class="btn-Add-To-Cart" onclick="AddToCartMethod('${item.id}')" >Add to Cart</button>
             </div>`;
-});
-itemsContainerElement.innerHTML = innerHTMLString;
+    });
+    itemsContainerElement.innerHTML = innerHTMLString;
+}
