9179b6de6900e52be1fc1b9cc6fc398ca77e9fa6
General-Monish <monissam28@gmail.com>
2026-03-04 07:08:41 +0530

    added: MyntraCloneFinal/data.js modified: MyntraCloneFinal/index.html modified: MyntraCloneFinal/script.js all items added using array of objects in data.js file and then imported it in index.html file and used it in script.js file to display the products on the webpage.

---
+++ MyntraCloneFinal/data.js
@@ -0,0 +1,58 @@
+const items = [
+  {
+    item_Image: "/MyntraCloneFinal/Assets/Images/mine/1.jpg",
+    rating: { stars: 4.5, reviews: "1400 Reviews" },
+    company_Name: "GTA Designs",
+    item_Name: "GTA Wallpaper 4k",
+    pricing: { discounted_Price: 499, original_Price: 999, discount_Percentage: 50 }
+  },
+  {
+    item_Image: "/MyntraCloneFinal/Assets/Images/mine/2.jpg",
+    rating: { stars: 4.5, reviews: "1400 Reviews" },
+    company_Name: "GTA Designs",
+    item_Name: "GTA Wallpaper 4k",
+    pricing: { discounted_Price: 499, original_Price: 999, discount_Percentage: 50 }
+  },
+  {
+    item_Image: "/MyntraCloneFinal/Assets/Images/mine/3.jpg",
+    rating: { stars: 4.5, reviews: "1400 Reviews" },
+    company_Name: "GTA Designs",
+    item_Name: "GTA Wallpaper 4k",
+    pricing: { discounted_Price: 499, original_Price: 999, discount_Percentage: 50 }
+  },
+  {
+    item_Image: "/MyntraCloneFinal/Assets/Images/mine/4.jpg",
+    rating: { stars: 4.5, reviews: "1400 Reviews" },
+    company_Name: "GTA Designs",
+    item_Name: "GTA Wallpaper 4k",
+    pricing: { discounted_Price: 499, original_Price: 999, discount_Percentage: 50 }
+  },
+  {
+    item_Image: "/MyntraCloneFinal/Assets/Images/mine/5.jpg",
+    rating: { stars: 4.5, reviews: "1400 Reviews" },
+    company_Name: "GTA Designs",
+    item_Name: "GTA Wallpaper 4k",
+    pricing: { discounted_Price: 499, original_Price: 999, discount_Percentage: 50 }
+  },
+  {
+    item_Image: "/MyntraCloneFinal/Assets/Images/mine/6.jpg",
+    rating: { stars: 4.5, reviews: "1400 Reviews" },
+    company_Name: "GTA Designs",
+    item_Name: "GTA Wallpaper 4k",
+    pricing: { discounted_Price: 499, original_Price: 999, discount_Percentage: 50 }
+  },
+  {
+    item_Image: "/MyntraCloneFinal/Assets/Images/mine/7.jpg",
+    rating: { stars: 4.5, reviews: "1400 Reviews" },
+    company_Name: "GTA Designs",
+    item_Name: "GTA Wallpaper 4k",
+    pricing: { discounted_Price: 499, original_Price: 999, discount_Percentage: 0 }
+  },
+  {
+    item_Image: "/MyntraCloneFinal/Assets/Images/mine/8.jpg",
+    rating: { stars: 0, reviews: "0 Reviews" },
+    company_Name: "GTA Designs",
+    item_Name: "GTA Wallpaper 4k",
+    pricing: { discounted_Price: 499, original_Price: 999, discount_Percentage: 50 }
+  }
+];
--- MyntraCloneFinal/script.js
+++ MyntraCloneFinal/script.js
@@ -1,35 +1,25 @@
 let itemsContainerElement = document.querySelector('.items-Container');
-let items = {
-    item_Image: "/MyntraCloneFinal/Assets/Images/mine/1.jpg",
-    rating:{
-        stars: 4.5,
-        reviews: "1400 Reviews"
-    },
-    company_Name: "GTA Designs",
-    item_Name: "GTA Wallpaper 4k",
-    pricing:{
-        discounted_Price: 499,
-        original_Price: 999,
-        discount_Percentage: 50
-    }
-}
-itemsContainerElement.innerHTML = `
- <div class="item-Container">
-                <img class="item-image-1" src="${items.item_Image}" alt="mine-image-1">
+let innerHTMLString = '';
+console.log(items);
+console.log(typeof items);
+items.forEach(item => {
+    innerHTMLString += `<div class="item-Container">
+                <img class="item-image-1" src="${item.item_Image}" alt="mine-image-1">
                 <div class="rating">
-                    ${items.rating.stars} ⭐ |${items.rating.reviews}
+                    ${item.rating.stars} ⭐ | ${item.rating.reviews}
                 </div>
                 <div class="company-Name">
-                    ${items.company_Name} 
+                    ${item.company_Name} 
                 </div>
                 <div class="item-Name">
-                    ${items.item_Name}
+                    ${item.item_Name}
                 </div>
                 <div class="pricing">
-                    <span class="discounted-price">₹ ${items.pricing.discounted_Price}</span>
-                    <span class="original-price">₹ ${items.pricing.original_Price}</span>
-                    <span class="discount-percentage">(${items.pricing.discount_Percentage} % OFF)</span>
+                    <span class="discounted-price">₹ ${item.pricing.discounted_Price}</span>
+                    <span class="original-price">₹ ${item.pricing.original_Price}</span>
+                    <span class="discount-percentage">(${item.pricing.discount_Percentage} % OFF)</span>
                 </div>
                 <button class="btn-Add-To-Cart">Add to Cart</button>
-            </div>
-`;
+            </div>`;
+});
+itemsContainerElement.innerHTML = innerHTMLString;
